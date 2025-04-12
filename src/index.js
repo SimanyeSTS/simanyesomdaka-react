6a2ed8f6634cd1a7e2c169dc832f74b27cfa113a
Simanye Somdaka <167189623+SimanyeSTS@users.noreply.github.com>
2025-04-12 19:35:16 +0200

    Update index.js (guaranteed loading screen)

--- src/index.js
+++ src/index.js
@@ -3,17 +3,29 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ModalProvider } from "./context/modal-context";
 import { ThemeProvider } from "./context/theme-context";
-import './index.css';
 
-// Create root and render the app
+// 1. Render loading screen IMMEDIATELY
+const loadingRoot = ReactDOM.createRoot(document.getElementById('loading-root'));
+loadingRoot.render(
+  <ThemeProvider>
+    <ModalProvider>
+      <App loadingMode />
+    </ModalProvider>
+  </ThemeProvider>
+);
+
+// 2. Render main app (will hydrate behind loading screen)
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <ModalProvider>
-        <App />
-      </ModalProvider>
-    </ThemeProvider>
-  </React.StrictMode>
+  <ThemeProvider>
+    <ModalProvider>
+      <App onLoaded={() => {
+        // Remove loading screen when EVERYTHING is ready
+        setTimeout(() => {
+          document.body.classList.remove('loading');
+          document.getElementById('loading-root').remove();
+        }, 300);
+      }} />
+    </ModalProvider>
+  </ThemeProvider>
 );

7f80dcd45ef9ac51b2d19b7c4146bb43b092aa33
SimanyeSTS <simanyesomdaka@gmail.com>
2025-10-23 16:49:30 +0200

    refined experience in about - 1½ years

--- src/sections/about/data.js
+++ src/sections/about/data.js
@@ -3,7 +3,7 @@ import { TbBooks } from "react-icons/tb";
 import { BiHappyHeartEyes } from "react-icons/bi";
 
 const data = [
-  { id: 1, icon: <FaAward />, title: "Experience", desc: "Junior Software Engineer with 2+ Years of Learning and Development" },
+  { id: 1, icon: <FaAward />, title: "Experience", desc: "Junior Software Engineer with 1½ Years of Learning and Development" },
   { id: 2, icon: <TbBooks />, title: "Projects", desc: "Several Projects Completed During Studies and Internships" },
   { id: 3, icon: <BiHappyHeartEyes />, title: "Clients", desc: "Eager to Build Lasting Relationships with Clients" },
 ];
